15817b69da54d002f5c6bf9b63445c9786b747ef
Jahiker <rojasjahiker@gmail.com>
2019-08-05 12:05:42 -0400

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,23 @@
 //=============================
 
 process.env.PORT = process.env.PORT || 3000;
+
+//=============================
+//ENTRONO
+//=============================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=============================
+//BASE DE DATOS
+//=============================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://jahiker:wB6*gdyK*KS&ZF9@cluster0-qarre.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
